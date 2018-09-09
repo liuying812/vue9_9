ca80c3b1892e1c4e6ba7b2b91dec6481478f818b
liuying <liuyinghpu@163.com>
2018-09-09 23:14:15 +0800

    mpvue

---
+++ mini-vue-answer/src/pages/counter/store.js
@@ -0,0 +1,28 @@
+// https://vuex.vuejs.org/zh-cn/intro.html
+// make sure to call Vue.use(Vuex) if using a module system
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment: (state) => {
+      const obj = state
+      obj.count += 1
+    },
+    decrement: (state) => {
+      const obj = state
+      obj.count -= 1
+    },
+    double: (state) => {
+      const obj = state
+      obj.count = obj.count * 2
+    }
+  }
+})
+
+export default store
